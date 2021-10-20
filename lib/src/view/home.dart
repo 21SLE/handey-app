f1f46eca676ccae1183b31d06c10675fb5c529c1
Hansle Lee <hssarah13@gmail.com>
2021-10-20 22:10:15 +0900

    home calendar place

--- lib/src/view/home.dart
+++ lib/src/view/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:handey_app/src/view/utils/screen_size.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -7,10 +8,27 @@ class Home extends StatelessWidget {
     return Scaffold(
       body: Container(
         alignment: Alignment.center,
-        child: Text(
-            'Home'
-        ),
+        child: Column(
+          children: [
+            CalendarWidget(),
+          ],
+        )
       ),
     );
   }
 }
+
+class CalendarWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ScreenSize size = ScreenSize();
+    return Container(
+      width: size.getSize(300.0),
+      height: size.getSize(54.0),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(size.getSize(6)),
+          color: Colors.yellow),
+    );
+  }
+}
+
